6ec8ecf389da20ebc84160d1cb43de1b019cdb20
Michał <michal_duda@onet.pl>
2020-03-28 21:11:17 +0100

    Dodanie get po id do punktu 4.6

--- Cwieczenie3/Cwieczenie3/Controllers/StudentsController.cs
+++ Cwieczenie3/Cwieczenie3/Controllers/StudentsController.cs
@@ -16,5 +16,19 @@ namespace Cwieczenie3.Controllers
         {
             return "Kowalski, Malewski, Andrzejeski";
         }
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            } else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+
+
+            return NotFound("Nie znaleziono studenta");
+        }
     }
 }
