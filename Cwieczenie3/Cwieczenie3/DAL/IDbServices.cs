4d45c62580badd05fb5b098c044512529d0f520a
Michał <michal_duda@onet.pl>
2020-03-29 07:21:45 +0200

    Zadanie 8. Dodanie interfejsu i mocka,do punktu 5 oraz gratis numer indeksu

---
+++ Cwieczenie3/Cwieczenie3/DAL/IDbServices.cs
@@ -0,0 +1,13 @@
+﻿using Cwieczenie3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cwieczenie3.DAL
+{
+    public interface IDbServices
+    {
+        public IEnumerable<Student> GetStudents();
+    }
+}
---
+++ Cwieczenie3/Cwieczenie3/DAL/MockIDbServices.cs
@@ -0,0 +1,30 @@
+﻿using Cwieczenie3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cwieczenie3.DAL
+{
+    public class MockIDbService : IDbServices
+    {
+        private static IEnumerable<Student> _students;
+
+        static MockIDbService ()
+        {
+            _students = new List<Student>
+            {
+                new Student{IdStudent = 1, FirstName="Jan", LastName="Kowalski" , IndexNumber = $"s{new Random().Next(1, 20000)}"},
+
+                new Student{IdStudent = 2, FirstName="Anna", LastName="Malewska", IndexNumber = $"s{new Random().Next(1, 20000)}" },
+
+                new Student{IdStudent = 3, FirstName="Andrzej", LastName="Andrzejewicz", IndexNumber = $"s{new Random().Next(1, 20000)}" }
+            };
+        }
+
+        public IEnumerable<Student> GetStudents()
+        {
+            return _students;
+        }
+    }
+}
