8ed707f567c105ac2c13d46031a6bc8d79ab9e03
Michał <michal_duda@onet.pl>
2020-05-08 23:05:49 +0200

    Zadanie 5 init

---
+++ Cwieczenie3/Cwieczenie3/Controllers/EnrollmentsController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cwieczenie3.Controllers
+{
+    [Route("api/enrollments")]
+    [ApiController]
+    public class EnrollmentsController : ControllerBase
+    {
+    }
+}
